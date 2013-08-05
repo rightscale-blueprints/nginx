280d8ad1eb827f00d15b25ea863c00def9bc76cd
Chris Fordham <chris@xhost.com.au>
2013-08-05 10:42:01 +1000

    Update generate_metadata.sh to remove .gitignore for runit cookbook.

--- generate_metadata.sh
+++ generate_metadata.sh
@@ -3,3 +3,5 @@
 # generate metadata.json for all upstream opscode cookbooks (metadata.json should not exist in development repositories)
 knife cookbook metadata -o cookbooks/ --all
 
+# additionally, remove known .gitignore files
+[ -e ./cookbooks/runit/.gitignore ] && rm ./cookbooks/runit/.gitignore
